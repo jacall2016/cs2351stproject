891bc5192df2c481f6061c5be0c8119c0330ae4c
James Call <jcall2016@byui.edu>
2023-09-14 20:32:09 -0600

    added code

---
+++ 1stproject.cpp
@@ -0,0 +1,45 @@
+using System;
+class GlazerCalc
+{
+    static void Main()
+    {
+        //writing hello world
+        System.Console.WriteLine("Hello, World!");
+
+        //diffining verriables
+        string name = "James Call";
+        string location = "Idaho";
+
+        //outputting varriables
+        System.Console.WriteLine($"My Name is {name}");
+        System.Console.WriteLine($"I am from {location}");
+
+        //finding and displaying current date
+        string date = DateTime.Now.ToString("dd/MM/YYYY");
+        System.Console.WriteLine($"the current date is {date}");
+
+        //finding and displaying number of days till Christmas
+        DateTime today = DateTime.Today;
+        DateTime christmas = new DateTime(today.Year, 12, 25);
+        TimeSpan timeUntilChristmas = christmas - today;
+        int daysUntilChristmas = timeUntilChristmas.Days;
+        System.Console.WriteLine($"There are {daysUntilChristmas} days until Christmas.");
+
+        //section 2.1 in the C# Programming Yellow
+        double width, height, woodLength, glassArea;
+        string widthString, heightString;
+        widthString = Console.ReadLine();
+        width = double.Parse(widthString);
+        heightString = Console.ReadLine();
+        height = double.Parse(heightString);
+        woodLength = 2 * (width + height) * 3.25;
+        glassArea = 2 * (width * height);
+        Console.WriteLine("The length of the wood is " +
+        woodLength + " feet");
+        Console.WriteLine("The area of the glass is " +
+        glassArea + " square metres");
+
+        Console.ReadKey();
+
+    }
+}
